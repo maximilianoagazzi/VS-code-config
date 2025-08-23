607aedbef9bbbea69c08b907bba45357952988ad
Maxi Agazzi <maximilianoagazzi2@gmail.com>
2025-08-23 15:51:17 -0300

    commit 1, prueba 1

---
+++ functions.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "header.h"
+
+int add(int a, int b) 
+{
+    return a + b;
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "header.h"
+
+int main () {
+    int a = 5;
+    int b = 11;
+    int sum = add(a, b);
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
